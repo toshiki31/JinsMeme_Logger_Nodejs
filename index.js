d9091b4747b30ba80da0793298e896a0d459cb88
mori toshiki <toshiki@morinoMacBook-Air.local>
2023-11-15 18:32:40 +0900

    add: まばたきでled制御

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const server = require("ws").Server;
 const ws_server = new server({ port: 5001 });
+const five = require("johnny-five");
+const board = new five.Board();
 var count = 0;
 
-ws_server.on("connection", (ws) => {
-  console.log("connected from client");
-  ws.on("message", function (message) {
-    if (message.indexOf("heartbeat") === -1) {
-      // メッセージを出力
-      // console.log(message);
+board.on("ready", function () {
+  ws_server.on("connection", (ws) => {
+    console.log("connected from client");
+    ws.on("message", function (message) {
+      if (message.indexOf("heartbeat") === -1) {
+        // メッセージを出力
+        // console.log(message);
 
-      //実際の処理する場合はparseして処理していきます
-      const obj = JSON.parse(message);
-      // console.log("--- obj --- \n", obj);
-      if (obj.blinkSpeed !== 0) {
-        count = count + 1;
-        console.log("count: ", count);
+        //実際の処理する場合はparseして処理していきます
+        const obj = JSON.parse(message);
+        // console.log("--- obj --- \n", obj);
+        if (obj.blinkSpeed !== 0) {
+          count = count + 1;
+          console.log("count: ", count);
+          var led = new five.Led(13);
+          if (count % 2 === 0) {
+            led.off();
+          } else {
+            led.on();
+          }
+        }
       }
-    }
+    });
   });
 });
