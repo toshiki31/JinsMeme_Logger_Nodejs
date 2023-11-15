76c14cad3ed17149f37f174c3b8af698c0e3530e
mori toshiki <toshiki@morinoMacBook-Air.local>
2023-11-15 16:42:04 +0900

    mod: 瞬きをカウントできるように修正

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const server = require("ws").Server;
 const ws_server = new server({ port: 5001 });
+var count = 0;
 
 ws_server.on("connection", (ws) => {
   console.log("connected from client");
   ws.on("message", function (message) {
     if (message.indexOf("heartbeat") === -1) {
-      //メッセージを出力
-      console.log(message);
+      // メッセージを出力
+      // console.log(message);
 
       //実際の処理する場合はparseして処理していきます
-      //const obj = JSON.parse(messeage);
+      const obj = JSON.parse(message);
+      // console.log("--- obj --- \n", obj);
+      if (obj.blinkSpeed !== 0) {
+        count = count + 1;
+        console.log("count: ", count);
+      }
     }
   });
 });
