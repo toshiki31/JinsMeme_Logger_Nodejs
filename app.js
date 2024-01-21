3ce58e2dd174cc820bd8185b46411a57d05e9d16
mori toshiki <toshiki@morinoMacBook-Air.local>
2024-01-21 15:18:31 +0900

    mod

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const WEBSOCKET_PORT = 5001;
 const SERIAL_PORT_PATH = "/dev/cu.usbmodem11401";
 
 const wsServer = new WebSocketServer({ port: WEBSOCKET_PORT });
-const serialPort = new SerialPort({ path: SERIAL_PORT_PATH, baudRate: 9600 });
+const serialPort = new SerialPort({ path: SERIAL_PORT_PATH, baudRate: 57600 });
 const parser = serialPort.pipe(new ReadlineParser({ delimiter: "\r\n" }));
 const csvWriter = createObjectCsvWriter({
   path: "airpuf_out.csv",
@@ -32,28 +32,30 @@ const init = () => {
       // 初回タイマー開始
       // setTimeout()の第２引数(ms)ごとに"OK"を送信
       timerId = setTimeout(write, 3000, "OK\n");
-      console.log(`Timer is on: ${timerId}`);
+      // console.log(`Timer is on: ${timerId}`);
 
       ws.on("message", function (message) {
         if (message.indexOf("heartbeat") === -1) {
           //実際の処理する場合はparseして処理
           const obj = JSON.parse(message);
           if (obj.blinkSpeed !== 0) {
+            // 瞬目したの処理
+            clearTimeout(timerId); // タイマー停止
+            // console.log(`Timer is off: ${timerId}\n**********`);
+            timerId = setTimeout(write, 3000, "OK\n"); // タイマー再開
+            // console.log(`B Timer is on: ${timerId}`);
+
             // 瞬目回数をカウント
             blinkCount = blinkCount + 1;
             const time = Date.now();
             const today = new Date(time);
             console.log("Blink count: ", blinkCount);
-            // console.log("Blink date: ", today);
+            console.log("Blink date: ", today);
             const record = {
               name: "blink",
               date: today,
             };
             records.push(record);
-            clearTimeout(timerId); // タイマー停止
-            console.log(`Timer is off: ${timerId}\n**********`);
-            timerId = setTimeout(write, 3000, "OK\n"); // タイマー再開
-            console.log(`B Timer is on: ${timerId}`);
           }
         }
       });
@@ -71,7 +73,7 @@ const init = () => {
 
 // シリアルポートからのデータを受信したときの処理
 parser.on("data", function (data) {
-  console.log("Data from serial port: " + data);
+  console.log("Arduino: " + data);
   pushCount = pushCount + 1;
   const time = Date.now();
   const today = new Date(time);
@@ -82,13 +84,13 @@ parser.on("data", function (data) {
     date: today,
   };
   records.push(record);
-  console.log(`Timer pushed: ${timerId}\n**********`);
+  // console.log(`Timer pushed: ${timerId}\n**********`);
   timerId = setTimeout(write, 3000, "OK\n");
-  console.log(`A Timer is on: ${timerId}`);
+  // console.log(`A Timer is on: ${timerId}`);
 });
 
 const write = (data) => {
-  console.log("Write to serial port: " + data);
+  console.log("Write: " + data);
   serialPort.write(Buffer.from(data), function (err, results) {
     if (err) {
       console.log("Error writing to serial port: " + err);
@@ -96,13 +98,5 @@ const write = (data) => {
     }
   });
 };
-
-// const sendMessage = async (isOn) => {
-//   setTimeout(write, 3000, "OK\n");
-//   if (!isOn) {
-//     clearTimeout(timerId);
-//     console.log("Timer is off");
-//   }
-// };
 
 init();
